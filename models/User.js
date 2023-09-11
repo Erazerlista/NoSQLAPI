59656459a8cd2054c6b46b36c4853cecf0c032a0
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-09-11 13:03:16 -0700

    added friend count

--- models/User.js
+++ models/User.js
@@ -1,5 +1,10 @@
 const { Schema, Types } = require('mongoose');
 
+function validateEmail(email) {
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+}
+
 const UserSchema = new Schema(
   {
     assignmentId: {
@@ -17,33 +22,35 @@ const UserSchema = new Schema(
       required: 'Please enter email...',
       unique: true,
       validate: {
-        validator: validateEmail,
+        validator: validateEmail, 
         message: 'Please fill in a valid email address',
       },
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        'Please fill a valid email address',
-      ],
     },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Thought', 
+        ref: 'Thought',
       },
     ],
     friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'User', 
+        ref: 'User',
       },
     ],
   },
   {
     toJSON: {
       getters: true,
+      virtuals: true, 
     },
     id: false,
   }
 );
+
+// friendcount section
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
 module.exports = UserSchema;
