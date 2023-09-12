9510e528fea086ba46a90f193dc2e549c96bea58
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-09-12 15:44:49 -0700

    updated code

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,9 @@ const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/userthoughtsDB';
 
 mongoose.connect(connectionString, {
-  useNewUrlParser: true,
+ useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useCreateIndex: true, 
 });
 
 module.exports = mongoose.connection;
